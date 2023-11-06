391bbf979904e9d9483ef4aa1cdef3cccf37f5dd
Yogesh Yadav <er.itsyogesh@gmail.com>
2023-11-07 00:07:32 +0530

    Project started, Worked on Schema

---
+++ src/main/java/com/lineagecode/invoicehub/InvoicehubApplication.java
@@ -0,0 +1,13 @@
+package com.lineagecode.invoicehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicehubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lineagecode/invoicehub/InvoicehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lineagecode.invoicehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
