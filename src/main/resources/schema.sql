0874e03355a7efebcf10a91da000b690869af6f1
Yogesh Yadav <er.itsyogesh@gmail.com>
2023-11-07 14:32:26 +0530

    Schema completed

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,7 +11,7 @@
 CREATE SCHEMA IF NOT EXISTS invoicehub;
 
 SET NAMES 'UTF8MB4';
-SET TIME_ZONE = 'Asia/Kolkata';
+SET TIME_ZONE = '+05:30';
 
 USE invoicehub;
 
@@ -55,6 +55,68 @@ CREATE TABLE UserRoles(
     FOREIGN KEY (role_id) REFERENCES Roles (id) ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT UQ_UserRoles_User_Id UNIQUE (user_id)
 );
+
+DROP TABLE IF EXISTS Events;
+
+CREATE TABLE Events(
+    id                          BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    type                        VARCHAR(50) NOT NULL CHECK(type IN ('LOGIN_ATTEMPT','LOGIN_ATTEMPT_FAILURE','LOGIN_ATTEMPT_SUCCESS','PROFILE_UPDATE','PROFILE_PICTURE_UPDATE','ROLE_UPDATE','ACCOUNT_SETTINGS_UPDATE','PASSWORD_UPDATE','MFA_UPDATE')),
+    description                 VARCHAR(255) NOT NULL,
+    CONSTRAINT UQ_type_Type UNIQUE (type)
+);
+
+DROP TABLE IF EXISTS UserEvents;
+
+CREATE TABLE UserEvents(
+    id                          BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id                     BIGINT UNSIGNED NOT NULL,
+    event_id                    BIGINT UNSIGNED NOT NULL,
+    device                      VARCHAR(100) DEFAULT NULL,
+    ip_address                  VARCHAR(100) DEFAULT NULL,
+    created_at                  DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES Events (id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+DROP TABLE IF EXISTS AccountVerifications;
+
+CREATE TABLE AccountVerifications(
+    id                          BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id                     BIGINT UNSIGNED NOT NULL,
+    url                         VARCHAR(255) NOT NULL,
+    --date                        DATETIME NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT UQ_AccountVerifications_User_Id UNIQUE (user_id),
+    CONSTRAINT UQ_AccountVerifications_url UNIQUE (url)
+
+);
+
+DROP TABLE IF EXISTS ResetPasswordVerifications;
+
+CREATE TABLE ResetPasswordVerifications(
+    id                          BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id                     BIGINT UNSIGNED NOT NULL,
+    url                         VARCHAR(255) NOT NULL,
+    expiration_date             DATETIME NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT UQ_ResetPasswordVerifications_User_Id UNIQUE (user_id),
+    CONSTRAINT UQ_ResetPasswordVerifications_url UNIQUE (url)
+
+);
+
+DROP TABLE IF EXISTS TwoFactorVerifications;
+
+CREATE TABLE TwoFactorVerifications(
+    id                          BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id                     BIGINT UNSIGNED NOT NULL,
+    code                        VARCHAR(10) NOT NULL,
+    expiration_date             DATETIME NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT UQ_TwoFactorVerifications_User_Id UNIQUE (user_id),
+    CONSTRAINT UQ_TwoFactorVerifications_code UNIQUE (code)
+
+);
+
 
 
 
